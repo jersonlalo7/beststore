0ee27b092e6f219038f24afe4a25f53ec3d1c7b1
Jerson Lalo <jersonlalo7@gmail.com>
2025-05-07 00:15:35 +0800

    Initial commit

---
+++ beststore/src/main/java/com/boostmytool/beststore/BeststoreApplication.java
@@ -0,0 +1,26 @@
+package com.boostmytool.beststore;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import com.boostmytool.beststore.repository.ProductsRepository;
+
+@SpringBootApplication
+public class BeststoreApplication {
+	
+	@Autowired
+	private ProductsRepository repo;
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeststoreApplication.class, args);
+	}
+	
+	@EventListener(ApplicationReadyEvent.class)
+    public void testRepo() {
+        System.out.println(repo.findAll());
+    }
+
+}
